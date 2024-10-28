b4d666d0371d6f6274ceca57cba9d1b86cf7e161
Shiwani Shah <shahshiwani246@gmail.com>
2024-10-28 14:06:31 +0545

    Form added in multiple laguages

---
+++ MultiLingual_Form/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MultiLingual_Form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLingual_Form.Controllers
+{
+    public class HomeController : MyController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.LanguageCulture = Thread.CurrentThread.CurrentUICulture.Name;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            return View(r);
+        }
+       
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageManage().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- MultiLingual_Form/Resource.Designer.cs
+++ MultiLingual_Form/Resource.Designer.cs
@@ -22,14 +22,14 @@ namespace MultiLingual_Form {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resource1 {
+    public class Resource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resource1() {
+        internal Resource() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace MultiLingual_Form {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MultiLingual_Form.Resource1", typeof(Resource1).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MultiLingual_Form.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
